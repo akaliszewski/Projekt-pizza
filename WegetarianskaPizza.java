d16820499e7be946b8d87c8618e9efa56e0341d3
akaliszewski <kaliszewski.adam@gmail.com>
2017-06-17 23:49:56 +0200

    First commit

---
+++ WegetarianskaPizza.java
@@ -0,0 +1,24 @@
+//Ta klasa dziedziczy pola z klasy Pizza
+public class WegetarianskaPizza extends Pizza{
+		
+	public String nazwa(){
+	return "Pizza Wegetariañska";
+	}
+	public String skladniki(){
+	return "";
+	}
+			
+	public int ostrosc(){
+	return 0;
+	}
+	
+	public int cena(){
+	return 0;
+	}
+	public int dowoz(){
+	return 0;
+	}
+}
+
+
+
